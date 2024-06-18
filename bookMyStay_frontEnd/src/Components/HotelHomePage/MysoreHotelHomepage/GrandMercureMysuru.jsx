7c70ed96db79270f2e63f2c69fe877f0d5858afc
Mahendra H L. <mahendra.hl@excelindia.com>
2024-06-18 09:27:50 +0530

    frontend folder modified

--- bookMyStay_frontEnd/src/Components/HotelHomePage/MysoreHotelHomepage/GrandMercureMysuru.jsx
+++ bookMyStay_frontEnd/src/Components/HotelHomePage/MysoreHotelHomepage/GrandMercureMysuru.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import HotelCarouselImages from "../HotelCarouselImages";
